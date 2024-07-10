562f43e2ecc31fcbd12b0d72d143e27031cfd5a9
Art Moskvin <amoskvin@me.com>
2024-07-10 15:54:59 +0200

    Remove unused imports

--- hide/__init__.py
+++ hide/__init__.py
@@ -1,3 +1,1 @@
-from .client.hide_client import CreateProjectRequest, File, FileInfo
 from .client.hide_client import HideClient as Client
-from .client.hide_client import Repository, Task, TaskResult
