c4ff9edd8b762005aa2420e81812d0e70ab1c5cb
Art Moskvin <amoskvin@me.com>
2024-09-18 14:37:00 +0200

    Add search (#20)

--- tests/test_hide_client.py
+++ tests/test_hide_client.py
@@ -2,18 +2,9 @@ from unittest.mock import Mock, patch
 
 import pytest
 
-from hide.client.hide_client import (
-    CreateProjectRequest,
-    File,
-    FileInfo,
-    HideClient,
-    HideClientError,
-    Project,
-    Repository,
-    Task,
-    TaskResult,
-)
-from hide.model import LineDiffUpdate, OverwriteUpdate, UdiffUpdate
+import hide
+from hide import model
+from hide.client import HideClientError
 
 PROJECT_ID = "123"
 PATH = "file.txt"
@@ -22,27 +13,27 @@ FILE = {"path": PATH, "lines": [{"number": 1, "content": CONTENT}]}
 
 
 @pytest.fixture
-def client() -> HideClient:
-    return HideClient(base_url="http://localhost")
+def client() -> hide.Client:
+    return hide.Client(base_url="http://localhost")
 
 
-def test_create_project_success(client: HideClient):
-    repository = Repository(url="http://example.com/repo.git")
-    request_data = CreateProjectRequest(repository=repository)
+def test_create_project_success(client: hide.Client):
+    repository = model.Repository(url="http://example.com/repo.git")
+    request_data = model.CreateProjectRequest(repository=repository)
     response_data = {"id": "123"}
 
     with patch("requests.post") as mock_post:
         mock_post.return_value = Mock(ok=True, json=lambda: response_data)
         project = client.create_project(repository=repository)
-        assert project == Project(id="123")
+        assert project == model.Project(id="123")
         mock_post.assert_called_once_with(
             "http://localhost/projects",
             json=request_data.model_dump(exclude_unset=True),
         )
 
 
-def test_create_project_failure(client: HideClient):
-    repository = Repository(url="http://example.com/repo.git")
+def test_create_project_failure(client: hide.Client):
+    repository = model.Repository(url="http://example.com/repo.git")
 
     with patch("requests.post") as mock_post:
         mock_post.return_value = Mock(ok=False, text="Error")
@@ -56,7 +47,7 @@ def test_get_tasks_success(client):
         mock_get.return_value = Mock(ok=True, json=lambda: response_data)
         tasks = client.get_tasks(PROJECT_ID)
         assert len(tasks) == 1
-        assert tasks[0] == Task(alias="build", command="make build")
+        assert tasks[0] == model.Task(alias="build", command="make build")
         mock_get.assert_called_once_with("http://localhost/projects/123/tasks")
 
 
@@ -72,7 +63,7 @@ def test_run_task_command_success(client):
     with patch("requests.post") as mock_post:
         mock_post.return_value = Mock(ok=True, json=lambda: response_data)
         result = client.run_task(PROJECT_ID, command="echo Hello")
-        assert result == TaskResult(stdout="output", stderr="", exit_code=0)
+        assert result == model.TaskResult(stdout="output", stderr="", exit_code=0)
         mock_post.assert_called_once_with(
             "http://localhost/projects/123/tasks", json={"command": "echo Hello"}
         )
@@ -83,7 +74,7 @@ def test_run_task_alias_success(client):
     with patch("requests.post") as mock_post:
         mock_post.return_value = Mock(ok=True, json=lambda: response_data)
         result = client.run_task(PROJECT_ID, alias="build")
-        assert result == TaskResult(stdout="output", stderr="", exit_code=0)
+        assert result == model.TaskResult(stdout="output", stderr="", exit_code=0)
         mock_post.assert_called_once_with(
             "http://localhost/projects/123/tasks", json={"alias": "build"}
         )
@@ -114,7 +105,7 @@ def test_create_file_success(client):
     with patch("requests.post") as mock_post:
         mock_post.return_value = Mock(ok=True, json=lambda: FILE)
         file = client.create_file(PROJECT_ID, PATH, CONTENT)
-        assert file == File.from_content(path=PATH, content=CONTENT)
+        assert file == model.File.from_content(path=PATH, content=CONTENT)
         mock_post.assert_called_once_with(
             "http://localhost/projects/123/files",
             json={"path": PATH, "content": CONTENT},
@@ -132,7 +123,7 @@ def test_get_file(client):
     with patch("requests.get") as mock_get:
         mock_get.return_value = Mock(ok=True, json=lambda: FILE)
         file = client.get_file(PROJECT_ID, PATH)
-        assert file == File.from_content(path=PATH, content=CONTENT)
+        assert file == model.File.from_content(path=PATH, content=CONTENT)
         mock_get.assert_called_once_with(
             url=f"http://localhost/projects/123/files/{PATH}",
             params={"startLine": None, "numLines": None},
@@ -143,7 +134,7 @@ def test_get_file_with_start_line(client):
     with patch("requests.get") as mock_get:
         mock_get.return_value = Mock(ok=True, json=lambda: FILE)
         file = client.get_file(PROJECT_ID, PATH, start_line=10)
-        assert file == File.from_content(path=PATH, content="Hello World")
+        assert file == model.File.from_content(path=PATH, content="Hello World")
         mock_get.assert_called_once_with(
             url=f"http://localhost/projects/123/files/{PATH}",
             params={"startLine": 10, "numLines": None},
@@ -154,7 +145,7 @@ def test_get_file_with_num_lines(client):
     with patch("requests.get") as mock_get:
         mock_get.return_value = Mock(ok=True, json=lambda: FILE)
         file = client.get_file(PROJECT_ID, PATH, num_lines=10)
-        assert file == File.from_content(path=PATH, content="Hello World")
+        assert file == model.File.from_content(path=PATH, content="Hello World")
         mock_get.assert_called_once_with(
             url=f"http://localhost/projects/123/files/{PATH}",
             params={"startLine": None, "numLines": 10},
@@ -171,8 +162,10 @@ def test_get_file_failure(client):
 def test_update_file_with_udiff_succeeds(client):
     with patch("requests.put") as mock_put:
         mock_put.return_value = Mock(ok=True, json=lambda: FILE)
-        file = client.update_file(PROJECT_ID, PATH, UdiffUpdate(patch="test-patch"))
-        assert file == File.from_content(path=PATH, content=CONTENT)
+        file = client.update_file(
+            PROJECT_ID, PATH, model.UdiffUpdate(patch="test-patch")
+        )
+        assert file == model.File.from_content(path=PATH, content=CONTENT)
         mock_put.assert_called_once_with(
             f"http://localhost/projects/123/files/{PATH}",
             json={"type": "udiff", "udiff": {"patch": "test-patch"}},
@@ -185,9 +178,9 @@ def test_update_file_with_linediff_succeeds(client):
         file = client.update_file(
             PROJECT_ID,
             PATH,
-            LineDiffUpdate(start_line=1, end_line=10, content="test-content"),
+            model.LineDiffUpdate(start_line=1, end_line=10, content="test-content"),
         )
-        assert file == File.from_content(path=PATH, content=CONTENT)
+        assert file == model.File.from_content(path=PATH, content=CONTENT)
         mock_put.assert_called_once_with(
             f"http://localhost/projects/123/files/{PATH}",
             json={
@@ -200,8 +193,10 @@ def test_update_file_with_linediff_succeeds(client):
 def test_update_file_with_overwrite_succeeds(client):
     with patch("requests.put") as mock_put:
         mock_put.return_value = Mock(ok=True, json=lambda: FILE)
-        file = client.update_file(PROJECT_ID, PATH, OverwriteUpdate(content=CONTENT))
-        assert file == File.from_content(path=PATH, content=CONTENT)
+        file = client.update_file(
+            PROJECT_ID, PATH, model.OverwriteUpdate(content=CONTENT)
+        )
+        assert file == model.File.from_content(path=PATH, content=CONTENT)
         mock_put.assert_called_once_with(
             f"http://localhost/projects/123/files/{PATH}",
             json={"type": "overwrite", "overwrite": {"content": CONTENT}},
@@ -212,7 +207,7 @@ def test_update_file_failure(client):
     with patch("requests.put") as mock_put:
         mock_put.return_value = Mock(ok=False, text="Error")
         with pytest.raises(HideClientError, match="Error"):
-            client.update_file(PROJECT_ID, PATH, UdiffUpdate(patch="test-patch"))
+            client.update_file(PROJECT_ID, PATH, model.UdiffUpdate(patch="test-patch"))
 
 
 def test_delete_file_success(client):
@@ -236,7 +231,48 @@ def test_list_files_success(client):
     with patch("requests.get") as mock_get:
         mock_get.return_value = Mock(ok=True, json=lambda: response_data)
         files = client.list_files(PROJECT_ID)
-        assert files == [FileInfo(path="README.md")]
+        assert files == [model.FileInfo(path="README.md")]
+        mock_get.assert_called_once_with(
+            url="http://localhost/projects/123/files", params={}
+        )
+
+
+def test_list_files_with_include_param(client):
+    response_data = [{"path": "src/main.py", "content": "print('Hello')"}]
+    with patch("requests.get") as mock_get:
+        mock_get.return_value = Mock(ok=True, json=lambda: response_data)
+        files = client.list_files(PROJECT_ID, include=["src/**/*.py"])
+        assert files == [model.FileInfo(path="src/main.py")]
+        mock_get.assert_called_once_with(
+            url="http://localhost/projects/123/files",
+            params={"include": ["src/**/*.py"]},
+        )
+
+
+def test_list_files_with_exclude_param(client):
+    response_data = [{"path": "README.md", "content": "# Project"}]
+    with patch("requests.get") as mock_get:
+        mock_get.return_value = Mock(ok=True, json=lambda: response_data)
+        files = client.list_files(PROJECT_ID, exclude=["*.py", "*.js"])
+        assert files == [model.FileInfo(path="README.md")]
+        mock_get.assert_called_once_with(
+            url="http://localhost/projects/123/files",
+            params={"exclude": ["*.py", "*.js"]},
+        )
+
+
+def test_list_files_with_include_and_exclude_params(client):
+    response_data = [{"path": "src/util.py", "content": "# Utility functions"}]
+    with patch("requests.get") as mock_get:
+        mock_get.return_value = Mock(ok=True, json=lambda: response_data)
+        files = client.list_files(
+            PROJECT_ID, include=["src/**/*.py"], exclude=["src/test_*.py"]
+        )
+        assert files == [model.FileInfo(path="src/util.py")]
+        mock_get.assert_called_once_with(
+            url="http://localhost/projects/123/files",
+            params={"include": ["src/**/*.py"], "exclude": ["src/test_*.py"]},
+        )
 
 
 def test_list_files_failure(client):
@@ -244,3 +280,159 @@ def test_list_files_failure(client):
         mock_get.return_value = Mock(ok=False, text="Error")
         with pytest.raises(HideClientError, match="Error"):
             client.list_files(PROJECT_ID)
+
+
+def test_search_files_default(client):
+    response_data = [
+        {"path": "src/main.py", "lines": [{"number": 1, "content": "Hello"}]}
+    ]
+    with patch("requests.get") as mock_get:
+        mock_get.return_value = Mock(ok=True, json=lambda: response_data)
+        files = client.search_files(PROJECT_ID, query="Hello")
+        assert files == [model.File.from_content(path="src/main.py", content="Hello")]
+        mock_get.assert_called_once_with(
+            f"http://localhost/projects/{PROJECT_ID}/search",
+            params={"query": "Hello", "type": "content"},
+        )
+
+
+def test_search_files_exact(client):
+    response_data = [
+        {
+            "path": "src/util.py",
+            "lines": [{"number": 1, "content": "def hello(): pass"}],
+        }
+    ]
+    with patch("requests.get") as mock_get:
+        mock_get.return_value = Mock(ok=True, json=lambda: response_data)
+        files = client.search_files(
+            PROJECT_ID,
+            query="hello",
+            search_mode=model.SearchMode.EXACT,
+        )
+        assert files == [
+            model.File.from_content(path="src/util.py", content="def hello(): pass")
+        ]
+        mock_get.assert_called_once_with(
+            f"http://localhost/projects/{PROJECT_ID}/search",
+            params={
+                "query": "hello",
+                "type": "content",
+                "exact": "",
+            },
+        )
+
+
+def test_search_files_regex(client):
+    response_data = [
+        {
+            "path": "src/util.py",
+            "lines": [{"number": 1, "content": "def hello(): pass"}],
+        }
+    ]
+    with patch("requests.get") as mock_get:
+        mock_get.return_value = Mock(ok=True, json=lambda: response_data)
+        files = client.search_files(
+            PROJECT_ID,
+            query="hello",
+            search_mode=model.SearchMode.REGEX,
+        )
+        assert files == [
+            model.File.from_content(path="src/util.py", content="def hello(): pass")
+        ]
+        mock_get.assert_called_once_with(
+            f"http://localhost/projects/{PROJECT_ID}/search",
+            params={
+                "query": "hello",
+                "type": "content",
+                "regex": "",
+            },
+        )
+
+
+def test_search_files_hidden(client):
+    response_data = [
+        {
+            "path": "src/util.py",
+            "lines": [{"number": 1, "content": "def hello(): pass"}],
+        }
+    ]
+    with patch("requests.get") as mock_get:
+        mock_get.return_value = Mock(ok=True, json=lambda: response_data)
+        files = client.search_files(
+            PROJECT_ID,
+            query="hello",
+            show_hidden=True,
+        )
+        assert files == [
+            model.File.from_content(path="src/util.py", content="def hello(): pass")
+        ]
+        mock_get.assert_called_once_with(
+            f"http://localhost/projects/{PROJECT_ID}/search",
+            params={
+                "query": "hello",
+                "type": "content",
+                "showHidden": "",
+            },
+        )
+
+
+def test_search_files_with_include(client):
+    response_data = [
+        {
+            "path": "src/util.py",
+            "lines": [{"number": 1, "content": "def hello(): pass"}],
+        }
+    ]
+    with patch("requests.get") as mock_get:
+        mock_get.return_value = Mock(ok=True, json=lambda: response_data)
+        files = client.search_files(
+            PROJECT_ID,
+            query="hello",
+            include=["src/**/*.py"],
+        )
+        assert files == [
+            model.File.from_content(path="src/util.py", content="def hello(): pass")
+        ]
+        mock_get.assert_called_once_with(
+            f"http://localhost/projects/{PROJECT_ID}/search",
+            params={
+                "query": "hello",
+                "type": "content",
+                "include": ["src/**/*.py"],
+            },
+        )
+
+
+def test_search_files_with_exclude(client):
+    response_data = [
+        {
+            "path": "src/util.py",
+            "lines": [{"number": 1, "content": "def hello(): pass"}],
+        }
+    ]
+    with patch("requests.get") as mock_get:
+        mock_get.return_value = Mock(ok=True, json=lambda: response_data)
+        files = client.search_files(
+            PROJECT_ID,
+            query="hello",
+            exclude=["src/**/*.py"],
+        )
+        assert files == [
+            model.File.from_content(path="src/util.py", content="def hello(): pass")
+        ]
+        mock_get.assert_called_once_with(
+            f"http://localhost/projects/{PROJECT_ID}/search",
+            params={
+                "query": "hello",
+                "type": "content",
+                "exclude": ["src/**/*.py"],
+            },
+        )
+
+
+def test_search_files_failure(client):
+    with patch("requests.get") as mock_get:
+        mock_get.return_value = Mock(ok=False, text="Error")
+        with pytest.raises(HideClientError, match="Error"):
+            client.search_files(PROJECT_ID, query="test")
--- tests/test_toolkit.py
+++ tests/test_toolkit.py
@@ -2,9 +2,9 @@ from unittest.mock import create_autospec
 
 import pytest
 
-from hide.client.hide_client import HideClient, Project
+from hide import Client
+from hide.model import File, FileInfo, Line, Project, Task, TaskResult
 from hide.toolkit.toolkit import Toolkit
-from hide.model import File, Line, Task, TaskResult, FileInfo
 
 PROJECT_ID = "123"
 PATH = "file.txt"
@@ -14,138 +14,165 @@ PATCH = "test-patch"
 
 @pytest.fixture
 def hide_client():
-    return create_autospec(HideClient)
+    return create_autospec(Client)
+
 
 @pytest.fixture
-def toolkit(hide_client: HideClient) -> Toolkit:
+def toolkit(hide_client: Client) -> Toolkit:
     project = Project(id=PROJECT_ID)
     return Toolkit(project=project, client=hide_client)
 
 
-def test_get_tasks_success(toolkit: Toolkit, hide_client: HideClient):
+def test_get_tasks_success(toolkit: Toolkit, hide_client: Client):
     hide_client.get_tasks.return_value = [Task(alias="build", command="make build")]
     tasks = toolkit.get_tasks()
     assert tasks == '[{"alias": "build", "command": "make build"}]'
 
 
-def test_get_tasks_failure(toolkit: Toolkit, hide_client: HideClient):
+def test_get_tasks_failure(toolkit: Toolkit, hide_client: Client):
     hide_client.get_tasks.side_effect = Exception("Error")
     tasks = toolkit.get_tasks()
-    assert tasks == 'Failed to get tasks: Error'
+    assert tasks == "Failed to get tasks: Error"
 
 
-def test_run_task_command_success(toolkit: Toolkit, hide_client: HideClient):
-    hide_client.run_task.return_value = TaskResult(stdout="output", stderr="", exit_code=0)
+def test_run_task_command_success(toolkit: Toolkit, hide_client: Client):
+    hide_client.run_task.return_value = TaskResult(
+        stdout="output", stderr="", exit_code=0
+    )
     result = toolkit.run_task(command="echo Hello")
-    assert result == 'exit code: 0\nstdout: output\nstderr: '
+    assert result == "exit code: 0\nstdout: output\nstderr: "
 
 
-def test_run_task_alias_success(toolkit: Toolkit, hide_client: HideClient):
-    hide_client.run_task.return_value = TaskResult(stdout="output", stderr="", exit_code=0)
+def test_run_task_alias_success(toolkit: Toolkit, hide_client: Client):
+    hide_client.run_task.return_value = TaskResult(
+        stdout="output", stderr="", exit_code=0
+    )
     result = toolkit.run_task(alias="build")
-    assert result == 'exit code: 0\nstdout: output\nstderr: '
+    assert result == "exit code: 0\nstdout: output\nstderr: "
 
 
-def test_run_task_failure(toolkit: Toolkit, hide_client: HideClient):
+def test_run_task_failure(toolkit: Toolkit, hide_client: Client):
     hide_client.run_task.side_effect = Exception("Error")
     result = toolkit.run_task(command="echo Hello")
-    assert result == 'Failed to run task: Error'
+    assert result == "Failed to run task: Error"
 
 
-def test_create_file_success(toolkit: Toolkit, hide_client: HideClient):
-    hide_client.create_file.return_value = File(path=PATH, lines=[Line(number=1, content=CONTENT)])
+def test_create_file_success(toolkit: Toolkit, hide_client: Client):
+    expected = File(path=PATH, lines=[Line(number=1, content=CONTENT)])
+    hide_client.create_file.return_value = expected
     file = toolkit.create_file(PATH, CONTENT)
-    assert file == f'File created: {PATH}\n1 | {CONTENT}\n'
+    assert file == f"File created:\n{expected}"
 
 
-def test_create_file_failure(toolkit: Toolkit, hide_client: HideClient):
+def test_create_file_failure(toolkit: Toolkit, hide_client: Client):
     hide_client.create_file.side_effect = Exception("Error")
     file = toolkit.create_file(PATH, CONTENT)
-    assert file == 'Failed to create file: Error'
+    assert file == "Failed to create file: Error"
 
 
-def test_apply_patch_success(toolkit: Toolkit, hide_client: HideClient):
-    hide_client.update_file.return_value = File(path=PATH, lines=[Line(number=1, content=CONTENT)])
+def test_apply_patch_success(toolkit: Toolkit, hide_client: Client):
+    expected = File(path=PATH, lines=[Line(number=1, content=CONTENT)])
+    hide_client.update_file.return_value = expected
     file = toolkit.apply_patch(PATH, PATCH)
-    assert file == f'File updated: {PATH}\n1 | {CONTENT}\n'
+    assert file == f"File updated:\n{expected}"
 
 
-def test_apply_patch_failure(toolkit: Toolkit, hide_client: HideClient):
+def test_apply_patch_failure(toolkit: Toolkit, hide_client: Client):
     hide_client.update_file.side_effect = Exception("Error")
     file = toolkit.apply_patch(PATH, PATCH)
-    assert file == 'Failed to apply patch: Error'
+    assert file == "Failed to apply patch: Error"
 
 
-def test_insert_lines_success(toolkit: Toolkit, hide_client: HideClient):
-    hide_client.get_file.return_value = File(path=PATH, lines=[Line(number=1, content=CONTENT)])
-    hide_client.update_file.return_value = File(path=PATH, lines=[Line(number=1, content="New line"), Line(number=2, content=CONTENT)])
+def test_insert_lines_success(toolkit: Toolkit, hide_client: Client):
+    hide_client.get_file.return_value = File(
+        path=PATH, lines=[Line(number=1, content=CONTENT)]
+    )
+    expected = File(
+        path=PATH,
+        lines=[Line(number=1, content="New line"), Line(number=2, content=CONTENT)],
+    )
+    hide_client.update_file.return_value = expected
     file = toolkit.insert_lines(PATH, 1, "New line")
-    assert file == f'File updated: {PATH}\n1 | New line\n2 | {CONTENT}\n'
+    assert file == f"File updated:\n{expected}"
 
 
-def test_insert_lines_failure(toolkit: Toolkit, hide_client: HideClient):
+def test_insert_lines_failure(toolkit: Toolkit, hide_client: Client):
     hide_client.get_file.side_effect = Exception("Error")
     file = toolkit.insert_lines(PATH, 1, "New line")
-    assert file == 'Failed to insert lines: Error'
+    assert file == "Failed to insert lines: Error"
 
 
-def test_replace_lines_success(toolkit: Toolkit, hide_client: HideClient):
-    hide_client.get_file.return_value = File(path=PATH, lines=[Line(number=1, content=CONTENT), Line(number=2, content="Old line")])
-    hide_client.update_file.return_value = File(path=PATH, lines=[Line(number=1, content=CONTENT), Line(number=2, content="New line")])
+def test_replace_lines_success(toolkit: Toolkit, hide_client: Client):
+    hide_client.get_file.return_value = File(
+        path=PATH,
+        lines=[Line(number=1, content=CONTENT), Line(number=2, content="Old line")],
+    )
+    expected = File(
+        path=PATH,
+        lines=[Line(number=1, content=CONTENT), Line(number=2, content="New line")],
+    )
+    hide_client.update_file.return_value = expected
     file = toolkit.replace_lines(PATH, 2, 3, "New line")
-    assert file == f'File updated: {PATH}\n1 | {CONTENT}\n2 | New line\n'
+    assert file == f"File updated:\n{expected}"
 
 
-def test_replace_lines_failure(toolkit: Toolkit, hide_client: HideClient):
+def test_replace_lines_failure(toolkit: Toolkit, hide_client: Client):
     hide_client.get_file.side_effect = Exception("Error")
     file = toolkit.replace_lines(PATH, 1, 2, "New line")
-    assert file == 'Failed to replace lines: Error'
+    assert file == "Failed to replace lines: Error"
 
 
-def test_append_lines_success(toolkit: Toolkit, hide_client: HideClient):
-    hide_client.get_file.return_value = File(path=PATH, lines=[Line(number=1, content=CONTENT)])
-    hide_client.update_file.return_value = File(path=PATH, lines=[Line(number=1, content=CONTENT), Line(number=2, content="New line")])
+def test_append_lines_success(toolkit: Toolkit, hide_client: Client):
+    hide_client.get_file.return_value = File(
+        path=PATH, lines=[Line(number=1, content=CONTENT)]
+    )
+    expected = File(
+        path=PATH,
+        lines=[Line(number=1, content=CONTENT), Line(number=2, content="New line")],
+    )
+    hide_client.update_file.return_value = expected
     file = toolkit.append_lines(PATH, "New line")
-    assert file == f'File updated: {PATH}\n1 | {CONTENT}\n2 | New line\n'
+    assert file == f"File updated:\n{expected}"
 
 
-def test_append_lines_failure(toolkit: Toolkit, hide_client: HideClient):
+def test_append_lines_failure(toolkit: Toolkit, hide_client: Client):
     hide_client.get_file.side_effect = Exception("Error")
     file = toolkit.append_lines(PATH, "New line")
-    assert file == 'Failed to append lines: Error'
+    assert file == "Failed to append lines: Error"
 
 
-def test_get_file_success(toolkit: Toolkit, hide_client: HideClient):
-    hide_client.get_file.return_value = File(path=PATH, lines=[Line(number=1, content=CONTENT)])
+def test_get_file_success(toolkit: Toolkit, hide_client: Client):
+    expected = File(path=PATH, lines=[Line(number=1, content=CONTENT)])
+    hide_client.get_file.return_value = expected
     file = toolkit.get_file(PATH)
-    assert file == f'```{PATH}\n1 | {CONTENT}\n```'
+    assert file == f"{expected}"
 
 
-def test_get_file_failure(toolkit: Toolkit, hide_client: HideClient):
+def test_get_file_failure(toolkit: Toolkit, hide_client: Client):
     hide_client.get_file.side_effect = Exception("Error")
     file = toolkit.get_file(PATH)
-    assert file == 'Failed to get file: Error'
+    assert file == "Failed to get file: Error"
 
 
-def test_delete_file_success(toolkit: Toolkit, hide_client: HideClient):
+def test_delete_file_success(toolkit: Toolkit, hide_client: Client):
     hide_client.delete_file.return_value = True
     file = toolkit.delete_file(PATH)
-    assert file == f'File deleted: {PATH}'
+    assert file == f"File deleted: {PATH}"
 
 
-def test_delete_file_failure(toolkit: Toolkit, hide_client: HideClient):
+def test_delete_file_failure(toolkit: Toolkit, hide_client: Client):
     hide_client.delete_file.side_effect = Exception("Error")
     file = toolkit.delete_file(PATH)
-    assert file == 'Failed to delete file: Error'
+    assert file == "Failed to delete file: Error"
 
 
-def test_list_files_success(toolkit: Toolkit, hide_client: HideClient):
+def test_list_files_success(toolkit: Toolkit, hide_client: Client):
     hide_client.list_files.return_value = [FileInfo(path="README.md")]
     files = toolkit.list_files()
-    assert files == 'README.md'
+    assert files == "README.md"
 
 
-def test_list_files_failure(toolkit: Toolkit, hide_client: HideClient):
+def test_list_files_failure(toolkit: Toolkit, hide_client: Client):
     hide_client.list_files.side_effect = Exception("Error")
     files = toolkit.list_files()
-    assert files == 'Failed to list files: Error'
+    assert files == "Failed to list files: Error"
